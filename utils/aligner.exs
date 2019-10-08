e082991d7991ad7d911e8d72cfc5bc3d8122ef7e
Skander Mzali <skanderm@gmail.com>
2019-10-07 22:49:47 -0400

    Add exec command for running alignments

--- utils/aligner.exs
+++ utils/aligner.exs
@@ -5,8 +5,52 @@ defmodule Aligner do
     pairs =
       Enum.map(text_names, fn text_name ->
         root_name = Path.rootname(text_name)
-        %{text_path: "data/text/raw/#{text_name}", audio_path: "data/audio/#{root_name}.mp3"}
+
+        %{
+          root_name: root_name,
+          text_path: "data/text/raw/#{text_name}",
+          audio_path: "data/audio/#{root_name}.mp3"
+        }
       end)
+
+    pairs
+    |> Enum.each(&align_file/1)
+  end
+
+  def align_file(%{text_path: text_path, audio_path: audio_path, root_name: root_name}) do
+    case File.exists?("data/text/aligned/#{root_name}.json") do
+      true ->
+        :ok
+
+      _ ->
+        run_alignment(text_path, audio_path)
+    end
+  end
+
+  def run_alignment(text_path, audio_path) do
+    root_name = text_path |> Path.basename() |> Path.rootname()
+
+    IO.puts("Running alignment for #{root_name}")
+    hash = exec("docker", ~w[run -Pd lowerquality/gentle])
+    port = exec("docker", ~w[port #{hash}]) |> String.split(":") |> List.last()
+
+    :timer.sleep(2000)
+
+    cmd =
+      ~w[-s -F audio=@#{audio_path} -F transcript=@#{text_path} 0.0.0.0:#{port}/transcriptions?async=false]
+
+    results = exec("curl", cmd)
+
+    File.write("data/text/aligned/#{root_name}.json", results)
+
+    IO.puts("Finished with #{root_name}")
+
+    exec("docker", ~w[stop #{hash}])
+  end
+
+  def exec(cmd, opts) do
+    {resp, 0} = System.cmd(cmd, opts)
+    String.trim(resp)
   end
 end
 
