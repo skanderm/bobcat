94cbac06205e7ed47b8237a8f46507626f79fb8a
Skander Mzali <skanderm@gmail.com>
2019-10-19 17:59:23 -0400

    Add create-react-app with tailwind.css

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Episode from "./components/Episode"
+
+function App() {
+  return (
+    <div>
+      <Episode />
+    </div>
+  )
+}
+
+export default App
---
+++ ui/src/components/Episode.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Episode() {
+  return <h2>hello</h2>
+}
+
+export default Episode
