d916f2835b98ffa6c4787bfd61e8920dd4dc697a
Skander Mzali <skanderm@gmail.com>
2019-10-19 14:38:35 -0400

    Add working spectrogram generator

--- utils/spectrogram_generator.py
+++ utils/spectrogram_generator.py
@@ -6,22 +6,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def generate_specs():
-    paths = [f'data/audio/wav/{filename}' for filename in os.listdir('data/audio/wav')]
-    for path in paths:
-        basename = os.path.basename(path)
-        rootname = os.path.splitext(basename)[0]
-        spec_path = f'data/spectrograms/{rootname}.jpg'
+    audio_paths = [f'data/audio/wav/{filename}' for filename in os.listdir('data/audio/wav')]
+    for audio_path in audio_paths:
+        process_audio(audio_path)
 
-        if not os.path.exists(spec_path):
-            create_spec(path, spec_path)
+def process_audio(audio_path):
+    basename = os.path.basename(audio_path)
+    rootname = os.path.splitext(basename)[0]
+    print(f"Generating spectrograms for {rootname}")
 
-def create_spec(fn_audio, fn_gram):
-    clip, sample_rate = librosa.load(fn_audio, sr=None)
-    S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
-    fig = plt.figure(figsize=tuple(reversed(S.shape)), dpi=1)
+    clip, sample_rate = librosa.load(audio_path, sr=None)
+
+    slice_len = 60 # seconds
+
+    for i, frames in enumerate(each_slice(sample_rate * slice_len, clip)):
+        spec_path = f'data/spectrograms/{rootname}/{rootname}_{i}.png'
+        if not os.path.isdir(f'data/spectrograms/{rootname}'):
+            os.mkdir(f'data/spectrograms/{rootname}')
+        if not os.path.isfile(spec_path):
+            print(f"spec {i}")
+            spec = librosa.feature.melspectrogram(y=np.array(frames), sr=sample_rate)
+            store_spec(spec, spec_path)
+
+def store_spec(spec, spec_path, zoom=1):
+    figsize = tuple(reversed(spec.shape))
+    fig = plt.figure(figsize=figsize, dpi=zoom)
     plt.gca().set_axis_off()
-    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-    fig.savefig(fn_gram, dpi=zoom, bbox_inches='tight', pad_inches=0)
+    librosa.display.specshow(librosa.power_to_db(spec, ref=np.max))
+    fig.savefig(spec_path, dpi=zoom, bbox_inches='tight', pad_inches=0)
     plt.close(fig)
+
+def each_slice(size, iterable):
+    """ Chunks the iterable into size elements at a time, each yielded as a list.
+
+    Example:
+      for chunk in each_slice(2, [1,2,3,4,5]):
+          print(chunk)
+
+      # output:
+      [1, 2]
+      [3, 4]
+      [5]
+    """
+    current_slice = []
+    for item in iterable:
+        current_slice.append(item)
+        if len(current_slice) >= size:
+            yield current_slice
+            current_slice = []
+    if current_slice:
+        yield current_slice
 
 generate_specs()
